59661f5a04f898ac927daa7ce043c59edcd1b0c5
haydardk <134811972+haydardk@users.noreply.github.com>
2024-02-29 16:17:12 +0300

    Add files via upload

--- LIDAR_D1/lidar.c
+++ LIDAR_D1/lidar.c
@@ -10,21 +10,25 @@ uint8_t lidar_buffer[];
 double Lidar_Mesafe ; 
 double Lidar_Guc ; 
 uint8_t Dist_H , Dist_L , Strength_L , Strength_H , Lidar_Mode; 
+uint32_t shortbuffer = [0x42, 0x57, 0x02 ,0x00 ,0x00 ,0x00 ,0x02 ,0x11]; // FOR MODE CHANGE
+uint32_t longbuffer = [0x42, 0x57, 0x02 ,0x00 ,0x00 ,0x00 ,0x07 ,0x11];
 
-void lidar_uart_setup(){
-    PIO pio = pio1; // PIO PIN NUMARASINA GÖRE AYARLANACAK
-    uint sm = 0;
-    uint offset = pio_add_program(pio, &lidar_program);
-    lidar_program_init(pio, sm, offset, PIO_RX_PIN, LIDAR_BAUD);
+void lidar_rt_uart_setup(){
+    uart_init(HARD_UART_INST, LIDAR_BAUD);
+    gpio_set_function(LIDAR_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(LIDAR_RX_PIN, GPIO_FUNC_UART);
 }
-
-void lidar_configurate(){
-    // BU FONKSİYON BLOĞU YALNIZCA BİR KEZ ÇALIŞTIRACAK
+void lidar_configurate(distancemode_t distmode ){
     /* Herhangi bir platform ile (STM32 ya da Pico) aşağıdaki mesajlar HEX kodu olarak UART 115200 baud rate ile lidara gönderilmeli 
         Distance Mode : 42 57 02 00 00 00 02 11 Short distance mode, applicable for 0-5m
+                        42 57 02 00 00 00 07 11 Long distance mode 1-12m
     */
+   if (distmode == SHORT){
+        uart_puts(HARD_UART_INST, (uint8_t*)shortbuffer);
+   }else if(distmode == LONG){
+        uart_puts(HARD_UART_INST, (uint8_t*)longbuffer);
+   }
 }
-
 void lidar_process(uint8_t buffer[]){
     Dist_L = buffer[2];
     Dist_H = buffer[3];
@@ -36,8 +40,16 @@ void lidar_process(uint8_t buffer[]){
 
     Lidar_Mode = buffer[6] // 02 Short(0-5m) - 07 Long (1-12m) 
 }
+/*  ******* THIS FUNCTIONS ARE JUST FOR PIO ******
 
-void lidar_read_s(uint8_t buffer[]){
+void lidar_uart_setup(){ // FOR PIO
+    PIO pio = pio1; // PIO PIN NUMARASINA GÖRE AYARLANACAK
+    uint sm = 0;
+    uint offset = pio_add_program(pio, &lidar_program);
+    lidar_program_init(pio, sm, offset, PIO_RX_PIN, LIDAR_BAUD);
+} 
+
+void lidar_read_s(uint8_t buffer[]){  
     buffer = lidar_program_gets(pio, sm);
 }
 
@@ -52,5 +64,5 @@ void lidar_read_c(uint8_t buffer[]){
     }else{
         lidar_read_c(buffer);
     }
-}
+} */
     
--- LIDAR_D1/lidar.h
+++ LIDAR_D1/lidar.h
@@ -1,8 +1,14 @@
 #define LIDAR_BAUD 115200
-#define HARD_UART_INST uart2 // AYARLANACAK
-#define PIO_RX_PIN 3 // AYARLANACAK
+#define HARD_UART_INST uart2 
+#define LIDAR_TX_PIN 2
+#define LIDAR_RX_PIN 3  // AYARLANACAK
 
-#define SHORT_MODE_MESSAGE 4257020000000211
+#define PIO_RX_PIN 3 // AYARLANACAK PIO 
+
+typedef enum distancemode_t{
+    SHORT = 0,
+    LONG = 1,
+}
 
 /*Byte0-1 Byte2 Byte3 Byte4 Byte5 Byte6 Byte7 Byte8
 0x59 59 Dist_L Dist_H Strength_L Strength_H Mode 0x00 CheckSum*/
@@ -12,9 +18,12 @@ automatically switchable by default*/
 /*42 57 02 00 00 00 02 11 Short distance mode, applicable for 0-5m
 42 57 02 00 00 00 07 11 Long distance mode, applicable for 1-12m */
 
+void lidar_configurate(distancemode_t distmode);
+void lidar_process(uint8_t* buffer);
+void lidar_rt_uart_setup(void);
+
+
 void lidar_uart_setup(void);
-void lidar_configurate(void);
-void lidar_process(uint8_t buffer[]);
-void lidar_read_c(uint8_t buffer[]);
-void lidar_read_s(uint8_t buffer[]);
+void lidar_read_c(uint8_t* buffer);
+void lidar_read_s(uint8_t* buffer);
 
